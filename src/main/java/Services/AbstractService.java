81d5784e6da9aebca45372499391286e2940ed6f
Roman Anankin <roman.anankin@dataart.com>
2020-10-16 19:19:29 +0300

    object deleted

--- src/main/java/Services/AbstractService.java
+++ src/main/java/Services/AbstractService.java
@@ -15,8 +15,6 @@ public abstract class AbstractService<K,T extends AbstractDao<K>> implements Ser
         this.entity = entity;
     }
 
-    Object object = new Object();
-
     @Override
     public K save(K t) {
             return entity
